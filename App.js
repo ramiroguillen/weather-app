92023deb7c793edbce67ddb31a4cc494049561b3
ramiroguillen <ramirojguillen@gmail.com>
2023-05-31 17:26:44 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+import CurrentWeather from './src/components/CurrentWeather'
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <CurrentWeather />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1 }
+})
+
+export default App
