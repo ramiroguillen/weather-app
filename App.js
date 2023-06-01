550e3b022bef87a4dee7bf87985da4d6a22bae84
ramiroguillen <ramirojguillen@gmail.com>
2023-06-01 13:37:47 -0300

    navigation provisional styles

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import CurrentWeather from './src/screens/CurrentWeather'
-import UpcomingWeather from './src/screens/UpcomingWeather'
-import CityInfo from './src/screens/CityInfo'
-
-const Tab = createBottomTabNavigator()
+import Tabs from './src/components/Tabs'
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name={'Current'} component={CurrentWeather} />
-        <Tab.Screen name={'Upcoming'} component={UpcomingWeather} />
-        <Tab.Screen name={'City'} component={CityInfo} />
-      </Tab.Navigator>
+      <Tabs />
     </NavigationContainer>
   )
 }
---
+++ src/components/Tabs.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Feather } from '@expo/vector-icons'
+import CurrentWeather from '../screens/CurrentWeather'
+import UpcomingWeather from '../screens/UpcomingWeather'
+import CityInfo from '../screens/CityInfo'
+
+const Tab = createBottomTabNavigator()
+
+const Tabs = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'grey',
+        tabBarStyle: {
+          backgroundColor: 'lightblue'
+        },
+        headerStyle: {
+          backgroundColor: 'lightblue'
+        },
+        headerTitleStyle: {
+          fontWeight: 'bold',
+          fontSize: 25,
+          color: 'tomato'
+        }
+      }}
+    >
+      <Tab.Screen
+        name={'Current Weather'}
+        component={CurrentWeather}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Feather
+              name={'droplet'}
+              size={25}
+              color={focused ? 'tomato' : 'black'}
+            />
+          )
+        }}
+      />
+      <Tab.Screen
+        name={'Upcoming Weather'}
+        component={UpcomingWeather}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Feather
+              name={'clock'}
+              size={25}
+              color={focused ? 'tomato' : 'black'}
+            />
+          )
+        }}
+      />
+      <Tab.Screen
+        name={'City Info'}
+        component={CityInfo}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Feather
+              name={'home'}
+              size={25}
+              color={focused ? 'tomato' : 'black'}
+            />
+          )
+        }}
+      />
+    </Tab.Navigator>
+  )
+}
+
+export default Tabs
